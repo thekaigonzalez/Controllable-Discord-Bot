afd2b2fba49cca1f867d04ff88a5992348e2c678
KD <gkai70264@gmail.com>
2022-04-28 00:15:34 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import discord
+import asyncio
+from discord.ext import commands, tasks
+
+from aioconsole.stream import ainput
+
+from termcolor import cprint
+
+client = commands.Bot(command_prefix="/")
+
+curr_channel = 969083846110879747
+
+@tasks.loop(seconds=2)
+async def thr():
+    t = await ainput()
+
+    t2 = t.split(" ");
+
+    await client.get_channel(curr_channel).send(t)
+
+@client.event
+async def on_ready():
+
+    print("Welcome to IRC-inspired discord bot controlling!")
+    cprint("Warning: This may be against TOS but I have no clue. Open an issue if you think it is and i will review. (Me, Kai Gonzalez)", 'yellow')
+
+    if not thr.is_running():
+        thr.start() 
+
+@client.event
+async def on_message(msg: discord.Message):
+    if (msg.channel.id == curr_channel and msg.author.display_name != client.user.display_name):
+        
+        cprint("[" + msg.author.display_name.lower() + " (" + str(datetime.now().month) + "/" + str(datetime.now().day) + "/" +
+         str(datetime.now().year) + ")]: " + msg.content, 'green')
+
+client.run("YOUR_TOKEN")
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Controllable Discord Bot
+
+<https://github.com/fourjr/discord-cli> but simpler.
+
+This is a discord bot which you can control, it with a 'WYSIWYG' principle.
+
+when you start the bot, load the token, then you are able to control it.
+
+You can control the bot by editing the token at line 38, you need to copy a channel id and paste it as an integer for curr_channel.
+
+After that, just start the bot, and it will handle the rest!
